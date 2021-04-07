adc01382e504c408b2f0eb118e523d97f541f694
Juan Helios García Guzmán <juanheliosheavy@gmail.com>
2021-04-07 14:25:07 +0200

    :hammer: no-rollup spec for druid

--- scripts/druid/postSpec.py
+++ scripts/druid/postSpec.py
@@ -1,10 +1,15 @@
 import requests
 import json
 import pathlib
+import sys
 path = pathlib.Path(__file__).parent.absolute()
 
+if len(sys.argv) < 2:
+    print("Must provide ingestion spec file")
+    sys.exit()
+
 druid = "http://localhost:8081/druid/indexer/v1/supervisor/"
-f = open("{}/supervisor-spec.json".format(path))
+f = open(str(sys.argv[1]))
 data = json.load(f)
 
 response = requests.post(druid, json=data)
